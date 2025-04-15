129a249505e41348cc62b623afa9d7bfafc5965d
KeeperJohn <36058486+KeepsBritania@users.noreply.github.com>
2025-04-15 22:32:15 +0800

    Add files via upload

---
+++ partials/marquee.php
@@ -0,0 +1,296 @@
+<div class="top-banner-marquee">
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+      <div class="banner-items">
+        <div class="banner-inner-item">
+          <h4>Real Time Water Monitoring System</h4>
+        </div>
+        <div class="banner-inner-item">
+        <h4>Using IoT Sensor & Digital Sanitation Management</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Geo Mapping</h4>
+        </div>
+        <div class="banner-inner-item">
+          <h4>Use cntrl + scroll to zoom the map</h4>
+        </div>
+      </div>
+</div>
